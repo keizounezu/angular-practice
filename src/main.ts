aecce52a86969350e475bc53ef2c8d5ff4c1e711
keizounezu <keizo.nezu@bizreach.co.jp>
2024-06-06 10:17:10 +0900

    apply msw

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
+async function prepare() {
+  const { worker } = await import('./mocks/browser')
+  return await worker.start();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// woker起動であるprepare()が完了してからplatformBrowserDynamic().bootstrapModule(AppModule);を実行する
+prepare()
+  .then(() => {
+    return platformBrowserDynamic().bootstrapModule(AppModule);
+  })
+  .catch((err) => console.error(err));
